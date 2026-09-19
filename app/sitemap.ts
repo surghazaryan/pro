cdf71b38430fba218a4b0afc48929029c1da62ac
surghazaryan <sghazarjan@mail.ru>
2026-09-19 19:01:37 +0400

    update commit url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://krak-pi.vercel.app';
+    const baseUrl = 'https://pro-eight-liard.vercel.app/';
 
     return [
         {
