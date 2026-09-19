7e1f5299c7734dff1d622616f847e9c305ba46af
surghazaryan <sghazarjan@mail.ru>
2026-09-19 19:08:50 +0400

    Update sitemap with additional pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,5 +10,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'daily',
             priority: 1,
         },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
     ];
 }
